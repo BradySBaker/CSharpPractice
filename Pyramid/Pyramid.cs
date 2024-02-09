cb20d64354c692bba0719e0a50ed591c4834b67f
Brady Baker <bradysamuelbaker@gmail.com>
2024-02-09 10:49:13 -0800

    Finished pyramid problem and setup repo

---
+++ Pyramid/Pyramid.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.NetworkInformation;
+
+namespace CSharpPractice.Pyramid
+{
+    internal class Program
+    {
+
+        static void printPyramid(int size)
+        {
+            int starAmount = (int)Math.Pow(size, 2);
+            int startCharAmount = size - 1;
+            int printedStarAmount = 0;
+            int curStarAmount = 1;
+            while (printedStarAmount < starAmount)
+            {
+                startCharAmount++;
+                for (int i = 0; i < startCharAmount; i++)
+                {
+                    if (i >= startCharAmount - curStarAmount)
+                    {
+                        if (i == startCharAmount - 1)
+                        {
+                            Console.WriteLine("*");
+                            curStarAmount += 2;
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                        printedStarAmount++;
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input Size: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                printPyramid(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
---
+++ Pyramid/readme.md
@@ -0,0 +1,2 @@
+# Pyramid Creator
+This is a simple practice problem that created a pyramid of *'s at a height inputed by the user. It correctly spaces them out giving it a pyramid shape.
